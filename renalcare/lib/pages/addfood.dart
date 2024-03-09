fcdaf7a5559f917c8fd18c70bf9b533570623fa7
aviiciii <laavesh1@gmail.com>
2024-03-09 23:31:11 +0530

    getting food data from firebase

--- renalcare/lib/pages/addfood.dart
+++ renalcare/lib/pages/addfood.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:renalcare/services/food_log_service.dart';
+
 
 class addFood extends StatefulWidget {
   const addFood({super.key});
@@ -10,18 +13,49 @@ class addFood extends StatefulWidget {
 class _addFoodState extends State<addFood> {
   @override
   Widget build(BuildContext context) {
+    final DatabaseService _databaseService = DatabaseService("ulix3a2rwO2qjIVAKmgM");
+
+    String today = DateFormat('dd-MM-yyyy').format(DateTime.now());
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(15.0),
-        child: ListView(
-          children: [
-            SizedBox(
-              height: 15,
-            ),
-            fooddate("Feb 29"),
-            foodcontainer("Breakfast", "Biriyani"),
-            foodcontainer("Lunch", "Roti"),
-          ],
+        
+
+        child: StreamBuilder(
+          stream: _databaseService.get_food_logs(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+
+            List logs = snapshot.data?.docs ?? [];
+
+            // get today logs
+            logs = logs.where((log) => log['date'] == today).toList();
+
+            if (logs.isEmpty) {
+              return const Center(
+                child: Text('No users found'),
+              );
+            }
+
+            return ListView(
+              children: [
+                SizedBox(
+                  height: 15,
+                ),
+                fooddate(today),
+                for (var log in logs)
+                  foodcontainer(log['food'], log['time']),
+                foodcontainer("Lunch", "Roti"),
+              ],
+            );
+          },
         ),
       ),
     );
